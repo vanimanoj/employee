0d5386ff04a8d14bcc2d686c2d1aaa7574c6a5b1
vanimnaoj <vanimanoj1997@gmail.com>
2022-02-08 12:41:17 +0530

    EmployeeApp

--- app/src/main/java/com/example/employeapp/SearchActivity.java
+++ app/src/main/java/com/example/employeapp/SearchActivity.java
@@ -3,6 +3,7 @@ package com.example.employeapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -11,7 +12,8 @@ import android.widget.Toast;
 public class SearchActivity extends AppCompatActivity {
 EditText ed1,ed2,ed3,ed4;
 AppCompatButton b1;
-String getcode;
+String getcode,getname,getdesignation,getmobileno;
+DbHelper mydb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +23,30 @@ String getcode;
         ed3=(EditText) findViewById(R.id.designation);
         ed4=(EditText) findViewById(R.id.mobileno);
         b1=(AppCompatButton) findViewById(R.id.search);
+        mydb=new DbHelper(this);
+        mydb.getWritableDatabase();
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
              getcode=ed1.getText().toString();
-                Toast.makeText(getApplicationContext(), getcode, Toast.LENGTH_SHORT).show();
+                Cursor c=mydb.SearchEmployee(getcode);
+                if (c.getCount()==0)
+                {
+                    Toast.makeText(getApplicationContext(), "NO RESULT", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                   while(c.moveToNext())
+                   {
+                     getname=c.getString(2);
+                     getdesignation=c.getString(3);
+                     getmobileno=c.getString(4);
+                   }
+                   ed2.setText(getname);
+                   ed3.setText(getdesignation);
+                   ed4.setText(getmobileno);
+                }
             }
         });
 
